41d3be63afd2382e48afe705240a2413cf4f6e3d
Tom Haskins-Vaughan <tom@templestreetmedia.com>
2011-08-08 11:37:18 -0400

    Added some subversion aliases

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -1,4 +1,9 @@
 alias vim='vim -c VSTreeExplore'
 
+# Subversion
+alias st='svn st --ignore-externals'
+alias svnUp='svn up --ignore-externals'
+
+# Git
 alias gst="git status"
 alias gru="git remote update"
