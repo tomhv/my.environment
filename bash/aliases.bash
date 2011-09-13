6ba8013fedcd326842765f3ac4df47fd329e813b
Tom Haskins-Vaughan <tom@templestreetmedia.com>
2011-09-13 17:07:43 -0400

    Adding timestamp to command prompt

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -11,4 +11,5 @@ alias gru="git remote update"
 alias ebash="sudo vim ~/.bashrc"
 alias evim="sudo vim /etc/vimrc"
 
-
+# [2011-05-13 17:05 EDT][tom@tom-laptop my.environment]$
+export PS1="[\$(date '+%Y-%M-%d %H:%M %Z')][\u@\h \W]\$ "
