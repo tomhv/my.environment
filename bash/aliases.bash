c83195adcdcea6d979dbf9603803c684b585eef2
Tom Haskins-Vaughan <tom@templestreetmedia.com>
2011-09-14 10:45:26 -0400

    Added color to commane prompt

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -12,4 +12,9 @@ alias ebash="sudo vim ~/.bashrc"
 alias evim="sudo vim /etc/vimrc"
 
 # [2011-05-13 17:05 EDT][tom@tom-laptop my.environment]$
-export PS1="[\$(date '+%Y-%M-%d %H:%M %Z')][\u@\h \W]\$ "
+STARTBLUE='\e[0;34m';
+ENDBLUE="\e[0m"
+STARTGREEN='\e[0;32m';
+ENDGREEN="\e[0m"
+#export PS1="$STARTCOLOR\u@\h \w> $ENDCOLOR"
+export PS1="[\$(date '+%Y-%M-%d %H:%M %Z')][$STARTGREEN\u$ENDGREEN@$STARTBLUE\h$ENDBLUE \W]\$ "
